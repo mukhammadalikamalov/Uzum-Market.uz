bf4df27c6f94f4547ed7b3abfe37582c0284f235
Muhammadali <kamolovmuhammadali342@gmail.com>
2024-02-29 07:33:46 +0500

    first commit

---
+++ my-vue-app/js/script.js
@@ -0,0 +1,219 @@
+let product = [
+    {
+        id: Math.random(),
+        name: "Smartfon Honor X6a, 4/128 GB, 6/128 GB, 5200 mA/soa",
+        price: 209,
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/cnemuk125kub33f5f2m0/t_product_540_high.jpg#1709015652203"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "Baxmalli to'plam RANIA: xalat va sochiqlar, yuz va hammom uchun",
+        price: 209,
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/ck1descvutvccfo27mn0/t_product_540_high.jpg#1709015652203"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "Smartfon Honor X6a, 4/128 GB, 6/128 GB, 5200 mA/soa",
+        price: 209,
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/ccv6he35a95unf121g5g/t_product_540_high.jpg#1709015652203"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "Smartfon Honor X6a, 4/128 GB, 6/128 GB, 5200 mA/soa",
+        price: 209,
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/cc8egs6ha881nt1ejt40/t_product_540_high.jpg#1709015652204"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "Smartfon Honor X6a, 4/128 GB, 6/128 GB, 5200 mA/soa",
+        price: 209,
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/ckie8okjvf2sulnv3an0/t_product_540_high.jpg#1709015652204"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "Smartfon Honor X6a, 4/128 GB, 6/128 GB, 5200 mA/soa",
+        price: 209,
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/cf0prk0l08k0o9qho880/t_product_540_high.jpg#1709015652204"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "Smartfon Honor X6a, 4/128 GB, 6/128 GB, 5200 mA/soa",
+        price: 209,
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/ce6u94gv1htd23aiovcg/t_product_540_high.jpg#1709015652204"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "Smartfon Honor X6a, 4/128 GB, 6/128 GB, 5200 mA/soa",
+        price: 209,
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/cccd1reha8813vtohgtg/t_product_540_high.jpg#1709015652205"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "Smartfon Honor X6a, 4/128 GB, 6/128 GB, 5200 mA/soa",
+        price: 209,
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/cc8ru0eha88139ljb7og/t_product_540_high.jpg#1709015652205"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "Smartfon Honor X6a, 4/128 GB, 6/128 GB, 5200 mA/soa",
+        price: 209,
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/cdkv9bv0tgqvlm59f1pg/t_product_540_high.jpg#1709015652205"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "Smartfon Honor X6a, 4/128 GB, 6/128 GB, 5200 mA/soa",
+        price: 209,
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/cj76g97hj8j7lmgfk3c0/t_product_540_high.jpg#1709015652205"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "Smartfon Honor X6a, 4/128 GB, 6/128 GB, 5200 mA/soa",
+        price: 209,
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/cd9vamn0tgqvlm58vrfg/t_product_540_high.jpg#1709015652206"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "Smartfon Honor X6a, 4/128 GB, 6/128 GB, 5200 mA/soa",
+        price: 209,
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/ccv66a35a95unf121ed0/t_product_540_high.jpg#1709015652206"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "Smartfon Honor X6a, 4/128 GB, 6/128 GB, 5200 mA/soa",
+        price: 209,
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/cc88ggeha8816arta5l0/t_product_540_high.jpg#1709015652206"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "Smartfon Honor X6a, 4/128 GB, 6/128 GB, 5200 mA/soa",
+        price: 209,
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/cllf7qtenntcj8a9u6jg/t_product_540_high.jpg#1709015652206"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "Smartfon Honor X6a, 4/128 GB, 6/128 GB, 5200 mA/soa",
+        price: 209,
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/cd467crb3ho5lmuq6he0/t_product_540_high.jpg#1709015652207"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "Smartfon Honor X6a, 4/128 GB, 6/128 GB, 5200 mA/soa",
+        price: 209,
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/cl9fm15ennt861ipjbog/t_product_540_high.jpg#1709015652207"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "Smartfon Honor X6a, 4/128 GB, 6/128 GB, 5200 mA/soa",
+        price: 209,
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/cc6t3uuha88ep89kl4mg/t_product_540_high.jpg#1709015652207"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "Smartfon Honor X6a, 4/128 GB, 6/128 GB, 5200 mA/soa",
+        price: 209,
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/cjhgsbcjvf2hn7fe00dg/t_product_540_high.jpg#1709015652207"],
+        category: 1,
+    },
+    {
+        id: Math.random(),
+        name: "Smartfon Honor X6a, 4/128 GB, 6/128 GB, 5200 mA/soa",
+        price: 209,
+        isFavorite: false,
+        isInCart: true,
+        images: ["https://images.uzum.uz/cfnlhpnhgiov1qicauj0/t_product_540_high.jpg#1709015652207"],
+        category: 1,
+    },
+];
+
+const container = document.querySelector(".grid");
+
+product.forEach((product) => {
+    const card = document.createElement("div");
+    card.classList.add("card", "bg-white", "rounded-lg", "overflow-hidden", "shadow-md");
+
+    const images = product.images
+        .map((image) => `<img src='${image}' alt='${product.name}' class='w-full h-84 object-cover'/>`)
+        .join("");
+
+    card.innerHTML = `
+    <div class='p-2 '>
+     <div>${images}
+     <label class="like-container">
+  <input checked="checked" type="checkbox">
+  <div class="checkmark">
+    <svg viewBox="0 0 256 256">
+    <rect fill="none" height="256" width="256"></rect>
+    <path d="M224.6,51.9a59.5,59.5,0,0,0-43-19.9,60.5,60.5,0,0,0-44,17.6L128,59.1l-7.5-7.4C97.2,28.3,59.2,26.3,35.9,47.4a59.9,59.9,0,0,0-2.3,87l83.1,83.1a15.9,15.9,0,0,0,22.6,0l81-81C243.7,113.2,245.6,75.2,224.6,51.9Z" stroke-width="20px" stroke="#FFF" fill="none"></path></svg>
+  </div>
+</label></div>
+     <h2>${product.name}</h2>
+     <p>${product.description}</p>
+     <p class="bg-yellow-300 w-24">${product.price}</p>
+     <div class="border h-8 w-8 rounded-full flex center
+     items-center ml-52">
+  <div data-v-1a3a46a8="" class="slot default"><svg data-v-40da8b10="" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="ui-icon  add-cart-icon">
+<path d="M8 10V8H6V12.5C6 12.7761 5.77614 13 5.5 13C5.22386 13 5 12.7761 5 12.5V7H8C8 4.59628 9.95227 3 12 3C14.0575 3 16 4.70556 16 7H19V19.5C19 20.3284 18.3284 21 17.5 21H12.5C12.2239 21 12 20.7761 12 20.5C12 20.2239 12.2239 20 12.5 20H17.5C17.7761 20 18 19.7761 18 19.5V8H16V10H15V8H9V10H8ZM12 4C10.4477 4 9 5.20372 9 7H15C15 5.29444 13.5425 4 12 4Z" fill="black"></path>
+<path d="M7.5 14C7.77614 14 8 14.2239 8 14.5V17H10.5C10.7761 17 11 17.2239 11 17.5C11 17.7761 10.7761 18 10.5 18H8V20.5C8 20.7761 7.77614 21 7.5 21C7.22386 21 7 20.7761 7 20.5V18H4.5C4.22386 18 4 17.7761 4 17.5C4 17.2239 4.22386 17 4.5 17H7V14.5C7 14.2239 7.22386 14 7.5 14Z" fill="black"></path>
+</svg></div>
+</div>
+    </div>
+   `;
+
+    container.appendChild(card);
+});
