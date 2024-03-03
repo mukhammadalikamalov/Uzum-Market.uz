37d5331a5c7a3eacb02eb2c2b11b3e87d12fe246
Muhammadali <kamolovmuhammadali342@gmail.com>
2024-03-03 09:48:20 +0500

    made a lot of changes

--- my-vue-app/js/script.js
+++ my-vue-app/js/script.js
@@ -2,6 +2,7 @@ let product = [
     {
         id: Math.random(),
         name: "Oshxona anjomlari to'plami Kukmara, Granit Ultra liniyasi AP",
+        description: "5.0 (43 sharh)",
         price: "47 880 so'm/oyiga",
         realprice: "598 000 so'm",
         sale: "399 000 so'm",
@@ -13,6 +14,7 @@ let product = [
     {
         id: Math.random(),
         name: "Ofis texnikalari uchun qog'oz varaqlari Sylvamo Svetotopy, A4f C07/3, 500 varaq",
+        description: "5.0 (2624 sharh)",
         price: "5 180 so'm/oyiga",
         realprice: "57 000 so'm",
         sale: "43 000 so'm",
@@ -24,6 +26,7 @@ let product = [
     {
         id: Math.random(),
         name: "Qahva Carte Noire, sublimatsiyalangan, 95 g",
+        description: "5.0 (146 sharh)",
         price: "6 000 so'm/oyiga",
         realprice: "85 000 so'm",
         sale: "55 000 so'm",
@@ -35,6 +38,7 @@ let product = [
     {
         id: Math.random(),
         name: "Tikuv mashinasi Janome HomeDecor 2077",
+        description: "5.0 (33 sharh)",
         price: "226 800 so'm/oyiga",
         realprice: "2 850 000 so'm",
         sale: "1 890 000 so'm",
@@ -46,6 +50,7 @@ let product = [
     {
         id: Math.random(),
         name: "Batareykalar Duracell SIMPLY AA 2X10CRD MON, 2 dona",
+        description: "5.0 (21 sharh)",
         price: "1 440 so'm/oyiga",
         realprice: "20 000 so'm",
         sale: "12 000 so'm",
@@ -57,6 +62,7 @@ let product = [
     {
         id: Math.random(),
         name: "Oshxona anjomlari to'plami Kukmara № 8 Mramornaya, 3 dona",
+        description: "4.9 (137 sharh)",
         price: "57 720 so'm/oyiga",
         realprice: "80 000 so'm",
         sale: "481 000 so'm",
@@ -68,6 +74,7 @@ let product = [
     {
         id: Math.random(),
         name: "O'g'il bolalar uchun kombinezon-ishton, velyur",
+        description: "5.0 (7 sharh)",
         price: "7 800 so'm/oyiga",
         realprice: "98 000 so'm",
         sale: "65 000 so'm",
@@ -79,6 +86,7 @@ let product = [
     {
         id: Math.random(),
         name: "Dezinfektsiya qiluvchi vosita Cillit Bang Dog'ga qarshi + jilo",
+        description: "4.9 (148 sharh)",
         price: "3 480 so'm/oyiga",
         realprice: "43 000 so'm",
         sale: "29 000 so'm",
@@ -90,6 +98,7 @@ let product = [
     {
         id: Math.random(),
         name: "Shampun L'Oreal Paris Elseve Gialuron to'ldiruvchisi, suvsizlangan va yupqa sochlar uchun, 250 ml",
+        description: "4.9 (9 sharh)",
         price: "3 224 so'm/oyiga",
         realprice: "38 000 so'm",
         sale: "27 000 so'm",
@@ -101,6 +110,7 @@ let product = [
     {
         id: Math.random(),
         name: "Soch uchun bo'yog'i L'Oreal Paris Excellence Cool Creme",
+        description: "4.9 (71 sharh)",
         price: "7 560 so'm/oyiga",
         realprice: "76 000 so'm",
         sale: "63 000 so'm",
@@ -112,6 +122,7 @@ let product = [
     {
         id: Math.random(),
         name: "Tashkilotchi, quti, shkafning tanasi, narsalar va o'yinchoqlarni saqlash uchun idish",
+        description: "4.7 (355 sharh)",
         price: "8 400 so'm/oyiga",
         realprice: "130 000 so'm",
         sale: "70 000 so'm",
@@ -123,6 +134,7 @@ let product = [
     {
         id: Math.random(),
         name: "Masofadan boshqariladigan, suv o'tkazmaydigan amfibiya mashinasi, boshqaruv pult va braslet",
+        description: "5.0 (358 sharh)",
         price: "37 800 so'm/oyiga",
         realprice: "450 000 so'm",
         sale: "315 000 so'm",
@@ -134,6 +146,7 @@ let product = [
     {
         id: Math.random(),
         name: "Konsiler yuz uchun, tonal tuzatuvchi, 3.5 g",
+        description: "4.9 (118 sharh)",
         price: "1 200 so'm/oyiga",
         realprice: "100 000 so'm",
         sale: "10 000 so'm",
@@ -145,6 +158,7 @@ let product = [
     {
         id: Math.random(),
         name: "Blinchik qovuradigan tova tutqich bilan Kukmara Trendy Style, malachite",
+        description: "4.8 (229 sharh)",
         price: "18 720 so'm/oyiga",
         realprice: "180 000 so'm",
         sale: "156 000 so'm",
@@ -156,6 +170,7 @@ let product = [
     {
         id: Math.random(),
         name: "Ayyoolar uchun jemper Lamita",
+        description: "4.9 (103 sharh)",
         price: "16 320 so'm/oyiga",
         realprice: "210 000 so'm",
         sale: "136 000 so'm",
@@ -167,6 +182,7 @@ let product = [
     {
         id: Math.random(),
         name: "Oshxona elektron tarozisi SF-400, 10 kg gacha",
+        description: "5.0 (404 sharh)",
         price: "6 840 so'm/oyiga",
         realprice: "150 000 so'm",
         sale: "65 000 so'm",
@@ -178,6 +194,7 @@ let product = [
     {
         id: Math.random(),
         name: "Radio boshqariladigan o'yinchoq, bolalar uchun mini dron, qo'l bilan boshqariladiga",
+        description: "4.9 (81 sharh)",
         price: "8 280 so'm/oyiga",
         realprice: "148 000 so'm",
         sale: "79 000 so'm",
@@ -189,6 +206,7 @@ let product = [
     {
         id: Math.random(),
         name: "Shishali choynak, bambuk qopqoqli va filtr prujinali",
+        description: "4.8 (175 sharh)",
         price: "5 040 so'm/oyiga",
         realprice: "190 000 so'm",
         sale: "42 000 so'm",
@@ -200,6 +218,7 @@ let product = [
     {
         id: Math.random(),
         name: "Quyultirilgan sut Alekseevskoe, 8.5%, 360 g",
+        description: "4.9 (38 sharh)",
         price: "3 960 so'm/oyiga",
         realprice: "34 000 so'm",
         sale: "33 000 so'm",
@@ -211,6 +230,7 @@ let product = [
     {
         id: Math.random(),
         name: "Islomiy erkaklar soati Al Harameen HA-6506",
+        description: "4.8 (426 sharh)",
         price: "40 560 so'm/oyiga",
         realprice: "450 000 so'm",
         sale: "338 000 so'm",
@@ -232,40 +252,52 @@ product.forEach((product) => {
         .join("");
 
     card.innerHTML = `
-    <div class='p-2 '>
-     <div>${images}
-     <label class="like-container">
-  <input checked="checked" type="checkbox">
-  <div class="checkmark">
-    <svg viewBox="0 0 256 256">
-    <rect fill="none" height="256" width="256"></rect>
-    <path d="M224.6,51.9a59.5,59.5,0,0,0-43-19.9,60.5,60.5,0,0,0-44,17.6L128,59.1l-7.5-7.4C97.2,28.3,59.2,26.3,35.9,47.4a59.9,59.9,0,0,0-2.3,87l83.1,83.1a15.9,15.9,0,0,0,22.6,0l81-81C243.7,113.2,245.6,75.2,224.6,51.9Z" stroke-width="20px" stroke="#FFF" fill="none"></path></svg>
+	  <div class='p-2 '>
+	   <div>${images}
+	   <label class="like-container">
+	<input checked="checked" type="checkbox">
+	<div class="checkmark">
+	  <svg viewBox="0 0 256 256">
+	  <rect fill="none" height="256" width="256"></rect>
+	  <path d="M224.6,51.9a59.5,59.5,0,0,0-43-19.9,60.5,60.5,0,0,0-44,17.6L128,59.1l-7.5-7.4C97.2,28.3,59.2,26.3,35.9,47.4a59.9,59.9,0,0,0-2.3,87l83.1,83.1a15.9,15.9,0,0,0,22.6,0l81-81C243.7,113.2,245.6,75.2,224.6,51.9Z" stroke-width="20px" stroke="#FFF" fill="none"></path></svg>
+	</div>
+  </label></div>
+	   <h2 class="text-sm text-gray-700">${product.name}</h2>
+       <div class="flex items-center"
+	   <!-- Rating -->
+  <div class="flex flex-row-reverse justify-end items-center">
+	<input id="hs-ratings-readonly-1" type="radio" class="peer -ms-5 size-5 bg-transparent border-0 text-transparent cursor-pointer appearance-none checked:bg-none focus:bg-none focus:ring-0 focus:ring-offset-0" name="hs-ratings-readonly" value="1">
+	<label for="hs-ratings-readonly-1" class="text-yellow-400 pointer-events-none ">
+	  <svg class="flex-shrink-0 size-5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+		<path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+	  </svg>
+	</label>
   </div>
-</label></div>
-     <h2 class="text-sm text-gray-700">${product.name}</h2>
-     <p>${product.description}
-     <!-- Rating -->
-<div class="flex flex-row-reverse justify-end items-center">
-  <input id="hs-ratings-readonly-1" type="radio" class="peer -ms-5 size-5 bg-transparent border-0 text-transparent cursor-pointer appearance-none checked:bg-none focus:bg-none focus:ring-0 focus:ring-offset-0" name="hs-ratings-readonly" value="1">
-  <label for="hs-ratings-readonly-1" class="text-yellow-400 pointer-events-none dark:text-gray-600">
-    <svg class="flex-shrink-0 size-5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-      <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-    </svg>
-  </label>
+  <p class="ml-2 text-xs text-gray-500">${product.description}</p>
 </div>
-<!-- End Rating --></p>
-     <p class="mt-4 bg-yellow-300 w-32">${product.price}</p>
-     <p class="line-through">${product.realprice}</p>
-     <p>${product.sale}</p>
-     <div class="border h-8 w-8 rounded-full flex center
-     items-center ml-52">
-  <div data-v-1a3a46a8="" class="slot default"><svg data-v-40da8b10="" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="ui-icon  add-cart-icon">
-<path d="M8 10V8H6V12.5C6 12.7761 5.77614 13 5.5 13C5.22386 13 5 12.7761 5 12.5V7H8C8 4.59628 9.95227 3 12 3C14.0575 3 16 4.70556 16 7H19V19.5C19 20.3284 18.3284 21 17.5 21H12.5C12.2239 21 12 20.7761 12 20.5C12 20.2239 12.2239 20 12.5 20H17.5C17.7761 20 18 19.7761 18 19.5V8H16V10H15V8H9V10H8ZM12 4C10.4477 4 9 5.20372 9 7H15C15 5.29444 13.5425 4 12 4Z" fill="black"></path>
-<path d="M7.5 14C7.77614 14 8 14.2239 8 14.5V17H10.5C10.7761 17 11 17.2239 11 17.5C11 17.7761 10.7761 18 10.5 18H8V20.5C8 20.7761 7.77614 21 7.5 21C7.22386 21 7 20.7761 7 20.5V18H4.5C4.22386 18 4 17.7761 4 17.5C4 17.2239 4.22386 17 4.5 17H7V14.5C7 14.2239 7.22386 14 7.5 14Z" fill="black"></path>
-</svg></div>
-</div>
-    </div>
-   `;
+  <!-- End Rating -->
+	   <p class="price mt-2 w-32">${product.price}</p>
+	   <p class="mt-6 line-through text-xs text-slate-400">${product.realprice}</p>
+	   <p>${product.sale}</p>
+       	   <div class="border h-8 w-8 rounded-full flex center
+	   items-center ml-52">
+	<div data-v-1a3a46a8="" class="slot default"><svg data-v-40da8b10="" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="ui-icon  add-cart-icon">
+  <path d="M8 10V8H6V12.5C6 12.7761 5.77614 13 5.5 13C5.22386 13 5 12.7761 5 12.5V7H8C8 4.59628 9.95227 3 12 3C14.0575 3 16 4.70556 16 7H19V19.5C19 20.3284 18.3284 21 17.5 21H12.5C12.2239 21 12 20.7761 12 20.5C12 20.2239 12.2239 20 12.5 20H17.5C17.7761 20 18 19.7761 18 19.5V8H16V10H15V8H9V10H8ZM12 4C10.4477 4 9 5.20372 9 7H15C15 5.29444 13.5425 4 12 4Z" fill="black"></path>
+  <path d="M7.5 14C7.77614 14 8 14.2239 8 14.5V17H10.5C10.7761 17 11 17.2239 11 17.5C11 17.7761 10.7761 18 10.5 18H8V20.5C8 20.7761 7.77614 21 7.5 21C7.22386 21 7 20.7761 7 20.5V18H4.5C4.22386 18 4 17.7761 4 17.5C4 17.2239 4.22386 17 4.5 17H7V14.5C7 14.2239 7.22386 14 7.5 14Z" fill="black"></path>
+  </svg></div>
+  </div>
+	  </div>
+	 `;
 
     container.appendChild(card);
+});
+
+// Button to open modal
+document.getElementById("openModalButton").addEventListener("click", () => {
+    document.getElementById("modal").classList.remove("hidden");
+});
+
+// Button to close modal
+document.getElementById("closeModalButton").addEventListener("click", () => {
+    document.getElementById("modal").classList.add("hidden");
 });
